01d56cb72f644a0523e407bf350debb0888dc284
Graham Rogers <grogers12487@gmail.com>
2014-01-17 16:04:33 -0600

    Demo: Two Way Binding -Done

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,6 +3,8 @@
 eventsApp.controller('EventController',
     function EventController($scope){
 
+
+        $scope.sortorder = 'name';
         $scope.event = {
             name: "Angular Boot Camp",
             date: "1/1/2013",
@@ -17,7 +19,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1 hr',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this session you will learn the ins and outs of directives...',
                     upVoteCount: 0
@@ -25,7 +27,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '30 mins',
+                    duration: 2,
                     level: 'Introductory',
                     abstract: 'This session will take a closer look at scopes.',
                     upVoteCount: 0
@@ -33,12 +35,20 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
-                    duration: '2 hours',
+                    duration: 4,
                     level: 'Intermediate',
                     abstract: 'Controllers are the beginning of everything Angular does...',
                     upVoteCount: 0
                 }
             ]
         }
+
+        $scope.upVoteSession = function(session){
+          session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session){
+            session.upVoteCount--;
+        };
     }
 );
