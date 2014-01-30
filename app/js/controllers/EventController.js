3f70cc6dab7c090c24846a600950ef2c4e210743
grahamcrackers <grogers12487@gmail.com>
2014-01-29 19:34:13 -0600

    downgraded angular and fixed http call

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -3,9 +3,7 @@
 eventsApp.controller('EventController',
     function EventController($scope, eventData){
         $scope.sortorder = 'name';
-        eventData.getEvent(function(event){
-            $scope.event = event;
-        });
+        $scope.event = eventData.getEvent();
 
         $scope.upVoteSession = function(session){
           session.upVoteCount++;
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,14 +1,18 @@
 
-eventsApp.factory('eventData', function($http, $log) {
+eventsApp.factory('eventData', function($http, $q) {
     return {
-        getEvent: function(successcb) {
-            $http({method: 'GET', url: '/data/event/1'}).
+        getEvent: function() {
+            var deferred = $q.defer();
+
+            $http({method: 'GET', url: '/data/event/1.json'}).
                 success(function(data,status,headers,config){
-                    successcb(data);
+                    deferred.resolve(data);
             }).
                 error(function(data,status,headers,config){
-                    $log.warn(data,status,headers,config);
+                    deferred.reject(status);
             });
+
+            return deferred.promise;
         }
     };
 });
