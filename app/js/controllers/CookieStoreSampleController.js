c9e32e15a52b539f0bd88606b593570a7b527c91
Graham Rogers <grogers12487@gmail.com>
2014-01-31 16:59:31 -0600

    completed the "Creating and Using Services" module

---
+++ app/js/controllers/CookieStoreSampleController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+eventsApp.controller('CookieStoreSampleController',
+    function CompileSampleController($scope, $cookieStore) {
+        $scope.event = {id: 1, name:"Ny Event"};
+
+        $scope.saveEventToCookie = function(event) {
+            $cookieStore.put('event', event);
+        };
+
+        $scope.getEventFromCookie = function() {
+            console.log($cookieStore.get('event'));
+        };
+
+        $scope.removeEventCookie = function() {
+            $cookieStore.remove('event');
+        };
+    }
+);
